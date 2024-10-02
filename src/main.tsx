3e46448a6c4098c7c8368d53e73afcd56ead5127
Leo-Andreas Ervik <6738905+leoervik@users.noreply.github.com>
2024-10-02 14:07:14 +0200

    implementer tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./routes/App.tsx";
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import "./index.css";
 import Launch from "./routes/launch/Launch.tsx";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
 const router = createBrowserRouter([
   {
@@ -16,8 +17,12 @@ const router = createBrowserRouter([
   }
 ]);
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router}/>
+    </QueryClientProvider>
   </React.StrictMode>
 );
--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,20 +1,29 @@
 import ErrorPage from "./ErrorPage.tsx";
-import {useSmartClient} from "../smart/SmartClient.ts";
 import SmartConfigValidation from "../components/SmartConfigValidation.tsx";
 import IdTokenValidation from "../components/IdTokenValidation.tsx";
 import UserValidation from "../components/UserValidation.tsx";
 import PatientValidation from "../components/PatientValidation.tsx";
 import EncounterValidation from "../components/EncounterValidation.tsx";
+import {useQuery} from "@tanstack/react-query";
+import {oauth2 as SMART} from "fhirclient";
 
 function App() {
-  const {client, error: smartError} = useSmartClient();
+  const {data: client, error, isLoading} = useQuery({
+    queryKey: ["smartClient"],
+    queryFn: () => {
+      return SMART.ready();
+    },
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
+    refetchInterval: false,
+    refetchIntervalInBackground: false,
+    refetchOnMount: false
+  });
 
-  const error = smartError;
   return <div>
     <p className="text-3xl text-center pb-5">NAV SMART on FHIR compliance test</p>
-    {error ?
-      <ErrorPage error={error}/> :
-      <div className="flex flex-col">
+    { isLoading && <p>Initializing SMART client...</p> }
+    {client && <div className="flex flex-col">
         <SmartConfigValidation client={client}/>
         <br/>
         <IdTokenValidation client={client}/>
@@ -24,8 +33,8 @@ function App() {
         <PatientValidation client={client}/>
         <br/>
         <EncounterValidation client={client}/>
-      </div>
-    }
+    </div>}
+    {error && <ErrorPage error={error.message}/>}
   </div>;
 }
 
