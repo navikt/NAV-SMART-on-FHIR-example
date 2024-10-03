d69ce241d77f11fb31724febbb337eb3e24e6825
Leo-Andreas Ervik <6738905+leoervik@users.noreply.github.com>
2024-10-03 10:59:25 +0200

    remove unused code

--- src/smart/SmartClient.ts
+++ src/smart/SmartClient.ts
@@ -7,23 +7,12 @@ export function useSmartClient() {
   const [client, setClient] = useState<Client | undefined>(undefined);
   const [error, setError] = useState<string | undefined>(undefined);
 
-
-
   useEffect(() => {
     SMART.ready().then((client: Client) => {
       console.debug("✅ The client is initialized and ready to fetch from the FHIR server:", client);
       setClient(client);
     }).catch(err => setError(handleError("Unable to initialise FHIR client", err)));
   }, []);
-
-  /* TODO - FHIR validation
-  client.getFhirRelease().then(release => {
-    if (release !== 4) {
-      setError(`The FHIR server must be version R4 to be compliant. Detected version is ${release}.`);
-    }
-    console.debug(`✅ FHIR server version is R${release}`);
-  }).catch(err => setError(handleError("Unable to get the FHIR version", err)));
-  */
 
   return {client, error};
 }
