7d9e74f9312b21c9365932eb3a8f69e5ebc9542d
Jørn-Are <ja.flaten91@gmail.com>
2025-04-03 11:31:06 +0200

    chore: sticky sidebar
    Co-authored-by: Karl O <k@rl.run>

--- src/components/layout/Page.tsx
+++ src/components/layout/Page.tsx
@@ -8,7 +8,9 @@ const Page = ({ children, sidebar }: PropsWithChildren<Props>) => {
   return (
     <div className="flex flex-row">
       <div className="grow m-4">{children}</div>
-      <div className="max-w-64 m-4 mt-0 pt-6 border-l border-l-gray-300 pl-4">{sidebar}</div>
+      <div className="max-w-64 m-4 mt-0 pt-10 border-l border-l-gray-300 pl-4 relative">
+        <div className="absolute top-10 sticky">{sidebar}</div>
+      </div>
     </div>
   )
 }
